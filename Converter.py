9d3e7f1ed4657c701d6f8a9ccc384c34927f219d
Lary Walburn II <127873103+Laryshane@users.noreply.github.com>
2023-03-14 12:07:59 -0500

    Update Converter.py

--- Converter.py
+++ Converter.py
@@ -1,4 +1,13 @@
-Mil_Trav = float(input("Miles Travled: "))
-Miles = Mil_Trav
-Kilometers = Miles * 1.609344
-print("Kilometers Travled:", Kilometers)
+Mil_Trav = float(input("Miles Travled: ")) # Made Float so user can input a fraction number.
+Miles = Mil_Trav # We want Miles to = useres Input.
+Kilometers = Miles * 1.609344 # There is about 1.609344 Kilometers in a Mile.
+print("Kilometers Travled:", Kilometers) #Outputs the users Miles converted to Kilometers.
+
+# All of this can be changed to:
+
+# km_Trav = float(input("Kilometers Travled: ")) #Made Float so user can input a fraction number.
+# km = km_Trav #We want Kilometers to = useres Input.
+# Miles = km * 0.62137119 #There is about 0.62137119 Miles in a Kilometer.
+# print("Miles Travled:", Miles) #Outputs the users Kilometers converted to Miles.
+
+#To convert Kilometers to Miles
